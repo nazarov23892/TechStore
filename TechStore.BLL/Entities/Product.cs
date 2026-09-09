808312ea23d38db52eba8a3e6781e7042d3747cc
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-09 08:54:15 +0500

    Проект бизнес-логики.

---
+++ TechStore.BLL/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace TechStore.BLL.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public required string Category { get; set; }
+}
