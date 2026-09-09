951bf06cfff33a8c8051b7f5eecce3e80fb02cee
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-09 14:27:42 +0500

    + EF-миграция.

--- TechStore.WebApi/Program.cs
+++ TechStore.WebApi/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TechStore.DAL.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("TechStore");
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opts =>
+    {
+        opts.UseSqlServer(connectionString);
+    });
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
