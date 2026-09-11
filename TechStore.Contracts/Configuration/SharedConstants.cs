ded9a834679a4a1920504b0a1f9217eda8990971
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-11 08:45:16 +0500

    🔨 Базовый URI апи товаров вынесен в константы.

---
+++ TechStore.Contracts/Configuration/SharedConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace TechStore.Contracts.Configuration;
+
+public static class SharedConstants
+{
+    public static class BaseUri
+    {
+        /// <summary>
+        /// Базовый URI апи товаров.
+        /// </summary>
+        public const string Products = "api/products";
+    }
+}
