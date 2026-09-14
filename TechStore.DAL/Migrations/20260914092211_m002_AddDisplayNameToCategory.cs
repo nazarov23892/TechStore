0e74755fd52fe595c176c74ac955c1a258dddb09
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-14 14:23:10 +0500

    + EF миграция.

---
+++ TechStore.DAL/Migrations/20260914092211_m002_AddDisplayNameToCategory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechStore.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class m002_AddDisplayNameToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "Key");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                newName: "IX_Categories_Key");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "Key",
+                table: "Categories",
+                newName: "Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_Key",
+                table: "Categories",
+                newName: "IX_Categories_Name");
+        }
+    }
+}
