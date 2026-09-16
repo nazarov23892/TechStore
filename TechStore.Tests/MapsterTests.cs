00341b9834785433f1e9168cc59f0b190c7a2bbd
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-16 12:18:21 +0500

    + Тест профилей маппинга.

---
+++ TechStore.Tests/MapsterTests.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+
+namespace TechStore.Tests;
+
+public class MapsterTests
+{
+    [Fact(DisplayName = "Mapster: Проверка профилей маппинга.")]
+    public void ShouldProperlyMapProfiles()
+    {
+        var mapperConfig = new TypeAdapterConfig
+        {
+            RequireDestinationMemberSource = true
+        };
+
+        mapperConfig.Scan(typeof(AL.Configuration.AppConstants).Assembly);
+        mapperConfig.Compile();
+
+        Assert.True(true);
+    }
+}
